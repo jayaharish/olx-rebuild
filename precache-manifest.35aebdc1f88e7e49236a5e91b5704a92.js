fb86a60e24db4fd5f43a349fa08b0bc37eafa9cf
jayaharish <61152197+jayaharish@users.noreply.github.com>
2020-07-11 13:44:06 +0530

    Updates

---
+++ precache-manifest.35aebdc1f88e7e49236a5e91b5704a92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9efe26c2655fdeaf5dd1b79724b643a8",
+    "url": "/olx-rebuild/index.html"
+  },
+  {
+    "revision": "6d35bf20413fdc043599",
+    "url": "/olx-rebuild/static/css/main.0c88cb14.chunk.css"
+  },
+  {
+    "revision": "baee086fbf34d31a12e7",
+    "url": "/olx-rebuild/static/js/2.d042e762.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/olx-rebuild/static/js/2.d042e762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d35bf20413fdc043599",
+    "url": "/olx-rebuild/static/js/main.6c92aefc.chunk.js"
+  },
+  {
+    "revision": "6aa2c65fe49726184a25",
+    "url": "/olx-rebuild/static/js/runtime-main.2bb39fd5.js"
+  },
+  {
+    "revision": "bb5557317677de08603590958e9ae595",
+    "url": "/olx-rebuild/static/media/product.bb555731.png"
+  }
+]);
